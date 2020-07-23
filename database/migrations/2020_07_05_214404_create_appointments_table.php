bccd7ebc37cc1bc7efa4d360df0a70a46d94a0f3
Tiffany <tiffany@Tiffanys-MacBook-Pro.local>
2020-07-23 09:30:32 -0500

    added migrations

---
+++ database/migrations/2020_07_05_214404_create_appointments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('types');
+            $table->dateTime('date_of_appointment');
+            $table->boolean('completed')->default(false);
+            $table->boolean('cancelled')->default(false);
+            $table->boolean('no_show')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
