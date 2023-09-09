c6b349a0d6d99970be810ca4c62ab653b50b7a2c
tthompson899 <tdthompson899@gmail.com>
2023-09-09 09:17:02 -0500

    generate date time appointments in 30 min intervals

--- database/factories/AppointmentFactory.php
+++ database/factories/AppointmentFactory.php
@@ -2,9 +2,9 @@
 
 namespace Database\Factories;
 
+use Carbon\CarbonInterval;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Carbon\Carbon;
-use Carbon\CarbonPeriod;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Appointment>
@@ -18,14 +18,32 @@ class AppointmentFactory extends Factory
      */
     public function definition()
     {
-        $appointmentDate = $this->faker->dateTimeBetween(Carbon::now(), '+2 years', 'America/Chicago')->format('Y-m-d H:i:s');
+        $appointmentDate = Carbon::parse($this->faker->unique()->dateTimeBetween(Carbon::now('America/Chicago'), '+2 years', 'America/Chicago'))->format('Y-m-d');
+        $halfHourInterval = $this->getHalfHourInterval();
+        $key = array_rand($halfHourInterval);
+        $time = $halfHourInterval[$key];
+        $newTimeInstance = Carbon::createFromFormat('H:i A', $time, 'America/Chicago')->format('H:i A');
+        $dateTimeAppointment = Carbon::parse($appointmentDate . ' ' . $newTimeInstance)->format('Y-m-d H:i A');
+
         return [
-            'date_of_appointment' => $appointmentDate,
+            'date_of_appointment' => $dateTimeAppointment,
             'completed' => $this->faker->boolean(false),
             'cancelled' => $this->faker->boolean(false),
             'no_show' => $this->faker->boolean(false),
             'created_at' => Carbon::now('America/Chicago'),
             'updated_at' => Carbon::now('America/Chicago')
         ];
+    }
+
+    private function getHalfHourInterval() {
+        $formatter = function ($time) {
+            if ($time % 3600 == 0) {
+            return date('H:i A', $time);
+            } else {
+            return date('H:i A', $time);
+            }
+        };
+        $halfHourSteps = range(28800, 35*1800, 1800);
+        return array_map($formatter, $halfHourSteps);
     }
 }
